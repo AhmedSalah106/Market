b42f3e418ea0d8a2b0095c720ce4fce4c4ed3794
Ahmed_Salah <120193080+AhmedSalah106@users.noreply.github.com>
2024-10-12 05:05:08 +0300

    Add project files.

---
+++ MyMarket/MyMarket.cpp
@@ -0,0 +1,222 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<set>
+#include<sstream>
+#include<conio.h>
+#include <cstdlib>
+#include<windows.h>
+using namespace std;
+
+
+
+
+class Order
+{
+public:
+
+
+	double getTotalPrice()
+	{
+		return totalPrice;
+	}
+
+	void setTotalPrice(double price)
+	{
+		totalPrice += price;
+	}
+	set<string> productSelled;
+
+	string toString()
+	{
+		stringstream ss;
+
+		ss << "total price = $" << totalPrice << "\n";
+
+		ss << "products sold : \n";
+
+		int counter = 0;
+
+		for (auto& it : productSelled)
+		{
+			ss << ++counter << " " << it << '\n';
+		}
+
+		return ss.str();
+	}
+
+private:
+
+	double totalPrice = 0;
+
+};
+
+
+class Product
+{
+public:
+
+
+	void setProductName(string ProductName)
+	{
+		productName = ProductName;
+	}
+
+	void setProductQuantity(int Quantity)
+	{
+		productQuantity = Quantity;
+	}
+
+	void setProductPrice(double Price)
+	{
+		oneProductPrice = Price;
+	}
+
+
+	string getProductName()
+	{
+		return productName;
+	}
+	double getProductPrice()
+	{
+		return oneProductPrice;
+	}
+	int getProductQuantity()
+	{
+		return productQuantity;
+	}
+
+	double getTotalProductPrice()
+	{
+		return getProductPrice() * getProductQuantity();
+	}
+
+private:
+
+	string productName;
+	int productQuantity;
+	double totalProductPrice = 0;
+	double oneProductPrice;
+
+};
+
+
+
+
+class Category
+{
+public:
+	map<string, Product*>productsOfCategory;
+
+
+	string getCategoryName()
+	{
+		return categoryName;
+	}
+
+	int getProductQuantity(string productName)
+	{
+		return productsOfCategory[productName]->getProductQuantity();
+	}
+
+	double getProductPrice(string productName)
+	{
+		return productsOfCategory[productName]->getProductPrice();
+	}
+
+	string getProductName(string productName)
+	{
+		return productsOfCategory[productName]->getProductName();
+	}
+
+	double getTotalProductPrice(string productName)
+	{
+		return productsOfCategory[productName]->getTotalProductPrice();
+	}
+
+
+	void setProductName(string product, string ProductName)
+	{
+		productsOfCategory[product]->setProductName(ProductName);
+	}
+
+	void setProductQuantity(string productName, int Quantity)
+	{
+		productsOfCategory[productName]->setProductQuantity(Quantity);
+	}
+
+	void setProductPrice(string productName, double Price)
+	{
+		productsOfCategory[productName]->setProductPrice(Price);
+	}
+
+	Product* getProduct(string productName)
+	{
+		return productsOfCategory[productName];
+	}
+
+private:
+
+	string categoryName;
+
+};
+
+
+
+class Market
+{
+public:
+
+
+	int getProductQuantity(string categoryName, string ProductName)
+	{
+		return marketCategory[categoryName]->getProductQuantity(ProductName);
+	}
+
+	double getProductPrice(string categoryName, string productName)
+	{
+		return marketCategory[categoryName]->getProductPrice(productName);
+	}
+
+	string getProductName(string categoryName, string productName)
+	{
+		return marketCategory[categoryName]->getProductName(productName);
+	}
+
+	double getTotalProductPrice(string categoryName, string productName)
+	{
+		return marketCategory[categoryName]->getTotalProductPrice(productName);
+	}
+
+	void setProductName(string categoryName, string Product, string productName)
+	{
+
+		marketCategory[categoryName]->setProductName(Product, productName);
+	}
+
+	void setProductQuantity(string categoryName, string Product, int productQuantity)
+	{
+		marketCategory[categoryName]->setProductQuantity(Product, productQuantity);
+	}
+
+	void setProductPrice(string categoryName, string Product, double productPrice)
+	{
+		marketCategory[categoryName]->setProductPrice(Product, productPrice);
+	}
+
+
+	map<string, Category*> marketCategory;
+
+private:
+
+};
+
+
+int main()
+{
+
+
+
+
+	return 0;
+}
